8a29466628c2b5c85d62ae9991d9e03812637a35
caiguojing <1010784344@qq.com>
2019-08-02 19:18:32 +0800

    haha

--- flaskstudy003/app.py
+++ flaskstudy003/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 from flask import render_template,request
-
+from flaskstudy003.models import User
 app = Flask(__name__)
 
 
@@ -13,6 +13,22 @@ def index():
 # 注册
 @app.route('/regist/',methods=['get','post'])
 def user_regist():
+
+    if request.method == 'POST':
+        # 获取查询参数
+        print(request.form)
+
+        user = User()
+        user.name = request.form['user_name']
+        user.pwd = request.form['user_pwd']
+        user.email = request.form['user_email']
+        user.age = request.form['user_age']
+        user.face = request.form['user_face']
+        user.birthday = request.form['user_birth']
+
+        # 注册完成之后，重定向到登录页面,并把用户名也带过去（携带查询参数(get 方法)）
+        return redirect(url_for('user_login',username = user.name ))
+
     return render_template('user_regist.html')
 
 # 登录
@@ -23,3 +39,4 @@ def user_login():
 if __name__ == '__main__':
     # 调试模式
     app.run(debug=True)
+    #邮箱：123@qq
---
+++ flaskstudy003/models.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+
+
+class User(object):
+    def __init__(self,name=None,pwd=None,email=None,age=None,birthday=None,face=None):
+        self.name = name
+        self.pwd = pwd
+        self.email = email
+        self.age = age
+        self.birthday = birthday
+        self.face = face
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
